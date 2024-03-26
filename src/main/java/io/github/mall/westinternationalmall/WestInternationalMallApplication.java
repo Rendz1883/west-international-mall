23a142ae573e6f5f8a866479723866e8dcbf20b6
rdz <2215522181@qq.com>
2024-03-26 22:53:41 +0800

    first commit

---
+++ src/main/java/io/github/mall/westinternationalmall/WestInternationalMallApplication.java
@@ -0,0 +1,13 @@
+package io.github.mall.westinternationalmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WestInternationalMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WestInternationalMallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/mall/westinternationalmall/WestInternationalMallApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.mall.westinternationalmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WestInternationalMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
